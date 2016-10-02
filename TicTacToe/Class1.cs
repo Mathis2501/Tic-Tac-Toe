a4a00366b20507ad80059b9b84c2056969c902d2
Andre <andr9528@edu.eal.dk>
2016-10-02 17:00:05 +0200

    Lektier til 03-10-2016

--- TicTacToe/Class1.cs
+++ TicTacToe/Class1.cs
@@ -6,5 +6,24 @@ namespace TicTacToe
 {
     class Class1
     {
-    }
+        char player = ' ';
+        int playing = 0;
+        while (playing == 0)
+	{
+        if (player == x)
+	{
+        // check if there is 3 is in a row, if true declare x winner and set playing to 1
+        // check if the player all ready has 3 pices in play, if true ask waht piece to move
+        // ask for a location of which the palyer want to place a piece, and check if it is the same one as there have just been removed from
+        // change player to o
+	}
+        else // if player == o
+	{
+        // check if there is 3 is in a row, if true declare o winner and set playing to 1
+        // check if the player all ready has 3 pices in play, if true ask waht piece to move
+        // ask for a location of which the palyer want to place a piece, and check if it is the same one as there have just been removed from
+        // change player to x       
+	}
+	}
+}
 }
